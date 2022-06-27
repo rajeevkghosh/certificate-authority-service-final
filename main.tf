2333d4319f54e940505b19a946470c33b447b2a0
Rajeev Ghosh <rajeevkghosh@gmail.com>
2022-06-27 16:48:26 +0530

    updated code

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "tls_cert_request" "example" {
 }
 
 resource "google_privateca_ca_pool" "default" {
-  name = "my-ca-pool"
+  name = "my-ca-pool-tf1"
   location = "us-central1"
   tier = "ENTERPRISE"
   project = "modular-scout-345114"
